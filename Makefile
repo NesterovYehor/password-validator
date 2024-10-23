d3017cbed146536015934fe64f588c61a9cd573e
Yehor Nesterov <nesterovyehorgithub@gmail.com>
2024-10-23 14:31:42 +0200

    Tests added

---
+++ Makefile
@@ -0,0 +1,9 @@
+test:
+	go test ./...
+
+fmt:
+	go fmt ./...
+
+vet:
+	go vet ./...
+
--- length.go
+++ length.go
@@ -8,86 +8,30 @@ const (
 	seqAlphabet  = "abcdefghijklmnopqrstuvwxyz"
 )
 
-func getLength(password string) int {
-	password = removeRepeatingChars(password)
-	password = removeSequence(password, seqNums)
-	password = removeSequence(password, seqKeyboard2)
-	password = removeSequence(password, seqKeyboard1)
-	password = removeSequence(password, seqKeyboard2)
-	password = removeSequence(password, seqAlphabet)
-	password = removeSequence(password, getReversedString(seqNums))
-	password = removeSequence(password, getReversedString(seqKeyboard0))
-	password = removeSequence(password, getReversedString(seqKeyboard1))
-	password = removeSequence(password, getReversedString(seqKeyboard2))
-	password = removeSequence(password, getReversedString(seqAlphabet))
-	return len(password)
-}
-
-func removeRepeatingChars(s string) string {
-	var prevPrev rune
-	var prev rune
-
-	runes := []rune(s)
-
-	for i := 0; i < len(runes); i++ {
-		rune := runes[i]
-		if rune == prev && rune == prevPrev {
-			runes = deleteRuneAt(runes, i)
-			i--
-		}
-		prevPrev = prev
-		prev = rune
-	}
-
-	return string(runes)
-}
-
 func removeSequence(s, seq string) string {
 	seqRunes := []rune(seq)
 	runes := []rune(s)
-	mathces := 0
-
+	matches := 0
 	for i := 0; i < len(runes); i++ {
 		for j := 0; j < len(seqRunes); j++ {
 			if i >= len(runes) {
 				break
 			}
-
 			r := runes[i]
 			r2 := seqRunes[j]
-
 			if r != r2 {
-				mathces = 0
+				matches = 0
 				continue
 			}
-
-			mathces++
-
-			if mathces > 2 {
+			// found a match, advance the counter
+			matches++
+			if matches > 2 {
 				runes = deleteRuneAt(runes, i)
 			} else {
 				i++
 			}
 		}
 	}
-	return string(runes)
-}
-
-func getReversedString(s string) string {
-	n := 0
-	runes := make([]rune, len(s))
-
-	for _, rune := range s {
-		runes[n] = rune
-		n++
-	}
-
-	runes = runes[0:n]
-
-	for i := 0; i < n/2; i++ {
-		runes[i], runes[n-1-i] = runes[n-1-i], runes[i]
-	}
-
 	return string(runes)
 }
 
@@ -100,4 +44,51 @@ func deleteRuneAt(runes []rune, i int) []rune {
 	runes[len(runes)-1] = 0
 	runes = runes[:len(runes)-1]
 	return runes
+}
+
+func getReversedString(s string) string {
+	n := 0
+	rune := make([]rune, len(s))
+	for _, r := range s {
+		rune[n] = r
+		n++
+	}
+	rune = rune[0:n]
+	// Reverse
+	for i := 0; i < n/2; i++ {
+		rune[i], rune[n-1-i] = rune[n-1-i], rune[i]
+	}
+	// Convert back to UTF-8.
+	return string(rune)
+}
+
+func removeRepeatingChars(s string) string {
+	var prevPrev rune
+	var prev rune
+	runes := []rune(s)
+	for i := 0; i < len(runes); i++ {
+		r := runes[i]
+		if r == prev && r == prevPrev {
+			runes = deleteRuneAt(runes, i)
+			i--
+		}
+		prevPrev = prev
+		prev = r
+	}
+	return string(runes)
+}
+
+func getLength(password string) int {
+	password = removeRepeatingChars(password)
+	password = removeSequence(password, seqNums)
+	password = removeSequence(password, seqKeyboard0)
+	password = removeSequence(password, seqKeyboard1)
+	password = removeSequence(password, seqKeyboard2)
+	password = removeSequence(password, seqAlphabet)
+	password = removeSequence(password, getReversedString(seqNums))
+	password = removeSequence(password, getReversedString(seqKeyboard0))
+	password = removeSequence(password, getReversedString(seqKeyboard1))
+	password = removeSequence(password, getReversedString(seqKeyboard2))
+	password = removeSequence(password, getReversedString(seqAlphabet))
+	return len(password)
 }
